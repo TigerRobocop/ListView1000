379e6f6e7e85f0c185116dacffbed8faf91694ed
LivSouza <liviarsouza@gmail.com>
2015-09-05 12:36:08 -0300

    updated style and app icons

--- app/src/main/java/com/training/lsouza/listview1000/ListActivity.java
+++ app/src/main/java/com/training/lsouza/listview1000/ListActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +20,32 @@ public class ListActivity extends android.app.ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_list);
+        //  setContentView(R.layout.activity_list);
 
         Intent intent = getIntent();
         int ctItems = intent.getIntExtra("COUNT", 1);
 
         mList = listItemsBuilder(ctItems);
 
-       setListAdapter(new ThousandsAdapter(this, mList));
+        setListAdapter(new ThousandsAdapter(this, mList));
+
+        Toast.makeText(this, "List Count: " + ctItems, Toast.LENGTH_LONG).show();
+
+            }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TextView txt = (TextView)v.findViewById(R.id.textView_name);
+        String name =  txt.getText().toString();
+        Toast.makeText(this, "Item clicked: " + name, Toast.LENGTH_LONG).show();
     }
 
-
-    private List<Item> listItemsBuilder(int ctItems){
+    private List<Item> listItemsBuilder(int ctItems) {
         List<Item> retorno = new ArrayList<>();
 
-        for(int i = 1; i <= ctItems; i++){
-            Item item = new Item(i + "", "Item #"+ i);
+        for (int i = 1; i <= ctItems; i++) {
+            Item item = new Item(i + "", "Item #" + i);
 
             retorno.add(item);
         }
